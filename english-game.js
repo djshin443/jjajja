450e190bc4ccec6d6dea9264068674588f566f9b
djshin443 <132671614+djshin443@users.noreply.github.com>
2025-08-03 18:07:32 +0900

    Update english-game.js

--- english-game.js
+++ english-game.js
@@ -673,7 +673,7 @@ function render() {
 				
 				// 지율이를 키위 위에 그리기
 				const jiyulData = pixelData.jiyul;
-				drawPixelSprite(jiyulData.idle, jiyulData.colorMap, player.x, player.y - player.height - 6);
+				drawPixelSprite(jiyulData.idle, jiyulData.colorMap, player.x, player.y - player.height - 5);
 				
 			} else if (gameState.selectedVehicle === 'whitehouse' && pixelData.whitehouse) {
 				const whData = pixelData.whitehouse;
